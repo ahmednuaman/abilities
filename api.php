4a2b4d399756bee6cce348f66997f12fd614aadb
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-16 11:02:21 +0000

    sorted api, time to test its ass

--- api.php
+++ api.php
@@ -2,10 +2,6 @@
 /**
 * API Class
 * Simply handles the calls to save test performance details once a test has finished.
-* It's auto-init'd so make sure you provide the follow POST params
-* name: varchar(255), a unique human readable name for the test, will be converted_to_underscores
-* description: text, a description of the test and the result value (eg type, such as 500ms)
-* value: blob, the result of the test in as a granular form as possible
 */
 class API
 {
@@ -26,6 +22,59 @@ class API
     {
         // let's connected to our db
         $this->_connect();
+
+        // handle our request
+        $this->_handle_request();
+    }
+
+    /**
+     * Adds a test result into our DB
+     *
+     * @param $device_id int the DB ID of the device entry
+     * @param $name string the name of the test, usually this is the JS test file name (minus .js)
+     * @param $description string the description of the test
+     * @param $value blob the resulting value of the test
+     */
+    private function _add_result($device_id, $name, $description, $value)
+    {
+        // run the insert query
+        $query = $this->_con->query('INSERT INTO ' . MYSQL_TABLE_TEST_RESULT . ' (`device_id`, `name`, `description`, `value`) \
+            VALUES ("' . $device_id . '", "' . $name . '", "' . $description . '", "' . $value . '")');
+
+        // return the insert ID, just for lulz
+        return $this->_con->insert_id;
+    }
+
+    /**
+     * Adds test results into our DB
+     * It also checks to see if a device has been registered yet
+     * This method accepts an array
+     *
+     * @param $results array an array of results
+     */
+    private function _add_results($results)
+    {
+        // try and get the device's id and user agent
+        $device = $this->_con->real_escape_string($_SERVER['HTTP_X_REQUEST_DEVICE']);
+        $user_agent = $this->_con->real_escape_string($_SERVER['HTTP_USER_AGENT']);
+
+        // check our db to see if this device exists
+        $device_id = $this->_get_device_id($device, $user_agent);
+
+        // loop through our results and add them to the db
+        foreach ($results as $result)
+        {
+            // sanatise the result
+            foreach ($result as $key => $value)
+            {
+                $result[$key] = $this->_con->real_escape_string($value);
+            }
+
+            $this->_add_result($device_id, $result['name'], $result['description'], $result['value']);
+        }
+
+        // return the device id as a response as a valid JSON string
+        echo '"' . $device_id . '"';
     }
 
     /**
@@ -43,6 +92,61 @@ class API
         if ($this->_con->connect_error)
         {
             die('Failed to connect to DB');
+        }
+    }
+
+    /**
+     * Gets or generates a device ID
+     *
+     * @param $device string the device ID, if any
+     * @param $user_agent string the browser user agent
+     */
+    private function _get_device_id($device, $user_agent)
+    {
+        // run el query
+        $query = $this->_con->query('SELECT `id` FROM ' . MYSQL_TABLE_DEVICE . ' WHERE device = "' . $device . '" AND user_agent = "' . $user_agent . '"');
+
+        if ($query->num_rows === 1)
+        {
+            // set the device id
+            $device_id = $query->fetch_object()->id;
+        }
+        else
+        {
+            // create a record for this device
+            $query = $this->_con->query('INSERT INTO ' . MYSQL_TABLE_DEVICE . ' (`device`, `user_agent`) VALUES ("' . $device . '", "' . $user_agent . '")');
+
+            $device_id = $this->_con->insert_id;
+        }
+
+        return $device_id;
+    }
+
+    /**
+     * Handles our request to the API
+     * Currently you can only POST test results
+     *
+     * @access private
+     */
+    private function _handle_request()
+    {
+        // check for post
+        if (empty($_POST))
+        {
+            // gracefully fail
+            throw new BadMethodCallException('$_GET requests not implemeted yet');
+        }
+
+        // handle our request, assuming all the requested vars are present
+        if (isset($_POST['results']))
+        {
+            // stick these in our db
+            $this->_add_results($_POST['results']);
+        }
+        else
+        {
+            // bad call
+            throw new UnexpectedValueException('Some $_POST requests seem to be missing');
         }
     }
 }
