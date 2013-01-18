bcacf8dc2770c2581a495583ccd0facd35c1b147
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-18 14:35:12 +0000

    updated to handle longer device_ids

--- structure.sql
+++ structure.sql
@@ -2,10 +2,10 @@
 CREATE TABLE `device` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_agent` varchar(1000) NOT NULL DEFAULT '',
-  `device` varchar(1000) DEFAULT NULL,
+  `device` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `USER_AGENT` (`user_agent`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'test_result'
 CREATE TABLE `test_result` (
@@ -13,7 +13,7 @@ CREATE TABLE `test_result` (
   `device_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `description` text NOT NULL,
-  `value` blob NOT NULL,
+  `value` longtext NOT NULL,
   `tested` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `DEVICE_ID` (`device_id`)
