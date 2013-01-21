f3b26404e6d3e03da5205e972ba7c794390a2278
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-21 09:38:38 +0000

    made bg white

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ require_once 'harness.php';
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0" />
+        <style>
+            body {
+                background: #ffffff;
+            }
+        </style>
         <title>Test harness</title>
     </head>
     <body>
