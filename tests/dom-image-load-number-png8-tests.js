8b334c6c0e0681e8a924724d11d933765aac7b60
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-24 17:37:03 +0000

    split tests

---
+++ tests/dom-image-load-number-png8-tests.js
@@ -0,0 +1,103 @@
+/*
+description: A test case to see what is the optimum amount of PNG8 to load at any one time
+*/
+
+helpers.loadScript('assets/js/vendor/q.js');
+
+var baseURL = 'assets/img/';
+var lastImg;
+
+function loadImage(ext)
+{
+    var dfd = new Q.defer();
+    var img = new Image();
+    var el = document.createElement('img');
+
+    img.onload = function()
+    {
+        el.src = img.src;
+
+        if (lastImg)
+        {
+            dumpArea.insertBefore(el, lastImg);
+        }
+        else
+        {
+            dumpArea.appendChild(el);
+        }
+
+        lastImg = el;
+
+        dfd.resolve();
+    }
+
+    img.src = baseURL + ext;
+
+    return dfd.promise;
+}
+
+function loadImages(ext, num, dfd)
+{
+    var promises = [ ];
+
+    for (var i = num - 1; i >= 0; i--)
+    {
+        promises.push(
+            loadImage(ext)
+        );
+    }
+
+    Q
+    .all(promises)
+    .done(function()
+    {
+        dfd.resolve();
+    });
+}
+
+suite
+.on('cycle', function()
+{
+    // clear the dump area
+    dumpArea.innerHTML = '';
+
+    // null last image
+    lastImg = null;
+})
+.add('dom-image-load-number#png8x10', {
+    defer: true,
+    fn: function(dfd)
+    {
+        loadImages('png8.png', 10, dfd);
+    }
+})
+.add('dom-image-load-number#png8x50', {
+    defer: true,
+    fn: function(dfd)
+    {
+        loadImages('png8.png', 50, dfd);
+    }
+})
+.add('dom-image-load-number#png8x100', {
+    defer: true,
+    fn: function(dfd)
+    {
+        loadImages('png8.png', 100, dfd);
+    }
+});
+
+// wait for q before we run
+var runInterval = setInterval(function()
+{
+    if ('Q' in window)
+    {
+        if ('defer' in Q)
+        {
+            clearTimeout(runInterval);
+
+            suite.run({
+                async: true
+            });
+        }
+    }
+}, 500);
