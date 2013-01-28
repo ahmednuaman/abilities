be60220c3669cb64e320f1b0c81c779074cd6676
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-28 09:59:41 +0000

    added db type for associating the test data with the type of test for display

--- structure.sql
+++ structure.sql
@@ -12,9 +12,10 @@ CREATE TABLE `test_result` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `device_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
+  `type` int(11) NOT NULL,
   `description` text NOT NULL,
   `value` longtext NOT NULL,
   `tested` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `DEVICE_ID` (`device_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=217 DEFAULT CHARSET=latin1;
