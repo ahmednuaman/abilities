192e92a8ad291a9b47bcb74ac6a7e0dedefa6236
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-29 15:26:08 +0000

    reordered bizniz

--- index.php
+++ index.php
@@ -9,81 +9,110 @@ require_once 'harness.php';
         <link rel="stylesheet" href="assets/css/styles.css">
         <title>Test harness</title>
     </head>
-    <body>
-        <div class="container">
-            <?php if ($harness->is_testing): ?>
-                <?php
-                // set el time
-                $time = time();
-                ?>
-                <h1>Currently testing: <?php echo $harness->current_test->name; ?></h1>
-                <p><?php echo $harness->current_test->description; ?></p>
-                <h3>Log</h3>
-                <pre id="log-area"></pre>
-                <div id="dump-area"></div>
-                <form id="test-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                    <input type="hidden" name="result[name]" value="<?php echo $harness->current_test->name; ?>">
-                    <input type="hidden" name="result[type]" value="<?php echo $harness->current_test->type; ?>">
-                    <input type="hidden" name="result[description]" value="<?php echo $harness->current_test->description; ?>">
-                    <input type="hidden" name="result[value]" id="test-result-value" value="">
-                </form>
-                <script src="assets/js/vendor/benchmark-1.0.0.js"></script>
-                <script src="assets/js/helpers.js?x=<?php echo $time; ?>"></script>
-                <script src="assets/js/config.js?x=<?php echo $time; ?>"></script>
-                <script src="<?php echo $harness->current_test->path; ?>?x=<?php echo $time; ?>"></script>
-            <?php else: ?>
-                <h1>Test harness</h1>
-                <?php if ($harness->tests_time): ?>
-                    <div>
-                        <h3>Tests complete</h3>
-                        <p>It tooks <?php echo strftime('%M:%S', $harness->tests_time); ?> (MM:SS) to complete the tests</p>
+    <body class="<?php echo $harness->is_testing ? 'testing' : 'not-testing' ?>">
+        <div class="navbar navbar-static-top">
+            <div class="navbar-inner">
+                <a href="index.php" class="brand">Abilities</a>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="page-header">
+                <h1>
+                    <?php if ($harness->is_testing): ?>
+                        <?php echo $harness->current_test->name; ?> <small>Currently running</small>
+                    <?php else: ?>
+                        Abilities
+                            <small>
+                                <?php if ($harness->tests_time): ?>
+                                    Tests completed in <?php echo strftime('%M:%S', $harness->tests_time); ?>
+                                <?php else: ?>
+                                    A stressful test harness
+                                <?php endif; ?>
+                            </small>
+                    <?php endif; ?>
+                </h1>
+            </div>
+            <div class="row-fluid">
+                <?php if ($harness->is_testing): ?>
+                    <?php
+                    // set el time
+                    $time = time();
+                    ?>
+                    <div class="span8">
+                        <p><?php echo $harness->current_test->description; ?></p>
+                        <h3>Log</h3>
+                        <pre id="log-area"></pre>
+                        <div id="dump-area"></div>
+                        <form id="test-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                            <input type="hidden" name="result[name]" value="<?php echo $harness->current_test->name; ?>">
+                            <input type="hidden" name="result[type]" value="<?php echo $harness->current_test->type; ?>">
+                            <input type="hidden" name="result[description]" value="<?php echo $harness->current_test->description; ?>">
+                            <input type="hidden" name="result[value]" id="test-result-value" value="">
+                        </form>
                     </div>
-                <?php endif ?>
-                <p><a href="#" id="run-all-tests" onclick="runAllTests();">Run all the tests!</a> or select some...</p>
-                <form id="all-tests-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                    <ul>
-                        <?php foreach ($harness->get_all_tests() as $test): ?>
-                            <li id="test">
-                                <label>
-                                    <input type="checkbox" name="tests[]" value="<?php echo $test->name; ?>">
-                                    <?php echo $test->name; ?>: <?php echo $test->description; ?>
-                                </label>
-                            </li>
-                         <?php endforeach; ?>
-                    </ul>
-                    <button type="submit">
-                        Run tests
-                    </button>
-                </form>
-                <script>
-                    function runAllTests()
+                <?php else: ?>
+                    <div class="span8">
+                        <p><a href="#" id="run-all-tests">Run all the tests!</a> or select some...</p>
+                        <form id="all-tests-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                            <ul>
+                                <?php foreach ($harness->get_all_tests() as $test): ?>
+                                    <li id="test">
+                                        <label>
+                                            <input type="checkbox" name="tests[]" value="<?php echo $test->name; ?>">
+                                            <?php echo $test->name; ?>: <?php echo $test->description; ?>
+                                        </label>
+                                    </li>
+                                 <?php endforeach; ?>
+                            </ul>
+                            <button type="submit" class="btn btn-primary">
+                                Run tests
+                            </button>
+                        </form>
+                    </div>
+                <?php endif; ?>
+                <div class="span4 device-info">
+                    <h3>Device information</h3>
+
+                </div>
+            </div>
+        </div>
+        <?php if ($harness->is_testing): ?>
+            <script src="assets/js/vendor/benchmark-1.0.0.js"></script>
+            <script src="assets/js/helpers.js?x=<?php echo $time; ?>"></script>
+            <script src="assets/js/config.js?x=<?php echo $time; ?>"></script>
+            <script src="<?php echo $harness->current_test->path; ?>?x=<?php echo $time; ?>"></script>
+        <?php else: ?>
+            <script>
+                function runAllTests()
+                {
+                    var els = document.getElementsByTagName('input');
+                    var form = document.getElementById('all-tests-form');
+                    var el;
+
+                    for (var i = els.length - 1; i >= 0; i--)
                     {
-                        var els = document.getElementsByTagName('input');
-                        var form = document.getElementById('all-tests-form');
-                        var el;
+                        el = els[i];
 
-                        for (var i = els.length - 1; i >= 0; i--)
-                        {
-                            el = els[i];
-
-                            el.checked = true;
-                        }
-
-                        form.submit();
+                        el.checked = true;
                     }
 
-                    var link = document.getElementById('run-all-tests');
+                    form.submit();
+                }
 
-                    link.focus();
-                </script>
-                <?php /*if (isset($_POST['autorun'])): ?>
-                    <?php if ($_POST['autorun'] == '1'): ?>
-                        <script>
-                            runAllTests();
-                        </script>
-                    <?php endif; ?>
-                <?php endif;*/ ?>
-            <?php endif; ?>
-        </div>
+                var link = document.getElementById('run-all-tests');
+
+                link.onclick = runAllTests;
+                link.onkeypress = runAllTests;
+
+                link.focus();
+            </script>
+            <?php /*if (isset($_POST['autorun'])): ?>
+                <?php if ($_POST['autorun'] == '1'): ?>
+                    <script>
+                        runAllTests();
+                    </script>
+                <?php endif; ?>
+            <?php endif;*/ ?>
+        <?php endif; ?>
     </body>
 </html>
