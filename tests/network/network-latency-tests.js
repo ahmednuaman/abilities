67b2aa64cb9b988ac619f0c75c75781921aecf57
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-03-18 08:11:00 +0000

    updated network-latency-tests

--- tests/network/network-latency-tests.js
+++ tests/network/network-latency-tests.js
@@ -23,41 +23,41 @@ suite
         loadImage('assets/img/jpegprog.jpg', dfd);
     }
 })
-.add('network-latency#aws-s3-http', {
-    defer: true,
-    fn: function(dfd)
-    {
-        loadImage('http://abilities.s3.amazonaws.com/jpegprog.jpg', dfd);
-    }
-})
-.add('network-latency#aws-s3-https', {
-    defer: true,
-    fn: function(dfd)
-    {
-        loadImage('https://abilities.s3.amazonaws.com/jpegprog.jpg', dfd);
-    }
-})
-.add('network-latency#rackspace-http', {
-    defer: true,
-    fn: function(dfd)
-    {
-        loadImage('http://81a4f7fff57b565079e0-f2edf0fdc6e30b62e12bcf12ddd876cc.r48.cf3.rackcdn.com/jpegprog.jpg', dfd);
-    }
-})
-.add('network-latency#rackspace-https', {
-    defer: true,
-    fn: function(dfd)
-    {
-        loadImage('https://ab29890fe84d7d5b95c5-f2edf0fdc6e30b62e12bcf12ddd876cc.ssl.cf3.rackcdn.com/jpegprog.jpg', dfd);
-    }
-})
-.add('network-latency#google-appengine-http', {
-    defer: true,
-    fn: function(dfd)
-    {
-        loadImage('http://static-gadgets.appspot.com/assets/img/jpegprog.jpg', dfd);
-    }
-})
+// .add('network-latency#aws-s3-http', {
+//     defer: true,
+//     fn: function(dfd)
+//     {
+//         loadImage('http://abilities.s3.amazonaws.com/jpegprog.jpg', dfd);
+//     }
+// })
+// .add('network-latency#aws-s3-https', {
+//     defer: true,
+//     fn: function(dfd)
+//     {
+//         loadImage('https://abilities.s3.amazonaws.com/jpegprog.jpg', dfd);
+//     }
+// })
+// .add('network-latency#rackspace-http', {
+//     defer: true,
+//     fn: function(dfd)
+//     {
+//         loadImage('http://81a4f7fff57b565079e0-f2edf0fdc6e30b62e12bcf12ddd876cc.r48.cf3.rackcdn.com/jpegprog.jpg', dfd);
+//     }
+// })
+// .add('network-latency#rackspace-https', {
+//     defer: true,
+//     fn: function(dfd)
+//     {
+//         loadImage('https://ab29890fe84d7d5b95c5-f2edf0fdc6e30b62e12bcf12ddd876cc.ssl.cf3.rackcdn.com/jpegprog.jpg', dfd);
+//     }
+// })
+// .add('network-latency#google-appengine-http', {
+//     defer: true,
+//     fn: function(dfd)
+//     {
+//         loadImage('http://static-gadgets.appspot.com/assets/img/jpegprog.jpg', dfd);
+//     }
+// })
 // .add('network-latency#google-appengine-https', {
 //     defer: true,
 //     fn: function(dfd)
